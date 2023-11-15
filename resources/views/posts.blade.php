c02a3c248fabbb8b38d2c99c13904fa3ec4fd723
Gabriela <gabbyoch3@gmail.com>
2023-11-15 11:34:07 +0300

    learning laravel

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>A blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/post-one">Post One</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, repudiandae? Neque totam tempore nesciunt aliquid odit, impedit minus obcaecati ab maxime illo corrupti dignissimos perspiciatis earum repellat, nulla omnis praesentium?
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/post-two">Post Two</a></h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, repudiandae? Neque totam tempore nesciunt aliquid odit, impedit minus obcaecati ab maxime illo corrupti dignissimos perspiciatis earum repellat, nulla omnis praesentium?
+            </p>
+        </article>
+
+        <article>
+            <h1><a href="/posts/post-three">Post Three</a></h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, repudiandae? Neque totam tempore nesciunt aliquid odit, impedit minus obcaecati ab maxime illo corrupti dignissimos perspiciatis earum repellat, nulla omnis praesentium?
+                </p>
+            </article>
+
+</body>
+</html>
